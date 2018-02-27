eb35fa5d1602b2aab6bddb9b824c52647283d057
Chris Almeida <run.revelations@gmail.com>
2018-02-26 20:54:11 -0800

    add instructions

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '../public/index.html'));
+});
+
+/* GET routes and descriptions. */
+router.get('/routes', function(req, res, next) {
+  res.render('index', {});
+});
+
+/* GET instructions. */
+router.get('/instructions', function(req, res, next) {
+  res.render('instructions', {});
 });
 
 module.exports = router;
